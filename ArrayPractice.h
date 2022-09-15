89280f82057e05e60e3136edeefabe295aad3be1
Justin Etu <71947079+justinetu@users.noreply.github.com>
2022-09-15 10:04:45 -0500

    Create ArrayPractice.h

---
+++ ArrayPractice.h
@@ -0,0 +1,6 @@
+#include<iostream>
+int FindMinMax(int values[], int size, int *m);
+int SumProductElements(int values[], int size, int *p);
+void SaveEven(int values[]);
+int OddandEven(int values[], int *P);
+void PrintArray(int values[], int size);
